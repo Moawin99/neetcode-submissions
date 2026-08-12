bcb98b1a226d7baf983942daf4eaf2c88014493e
Mark <47339537+Moawin99@users.noreply.github.com>
2026-08-11 20:40:14 -0400

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for x in nums:
+            count[x] = count.get(x, 0) + 1
+        
+        # sorting the list, reversing it so it's decending
+        # Then grabbing only the first K numbers
+        max_sorted = (sorted(count.values())[::-1])[:k]
+
+        most = []
+
+        for x in nums:
+            if count[x] in max_sorted and x not in most:
+                most.append(x)
+        
+        return most
